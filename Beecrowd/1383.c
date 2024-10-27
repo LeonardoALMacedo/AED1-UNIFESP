62db03c10b5757002475cc896c4dcefdbf8a420e
Leonardo Arantes Lopes Macedo <macedoleonardo870@gmail.com>
2024-10-27 17:06:57 -0300

    Sudoku

--- Beecrowd/1383.c
+++ Beecrowd/1383.c
@@ -1,127 +1,130 @@
 /*
-
-CODIGO NAO CONCLUIDO AINDA
-
 Período Noturno
 Leonardo Arantes Lopes Macedo
 RA: 176558
 macedoleonardo870@gmail.com
 */
 
+
 #include <stdio.h>
 
-int verify (int* bool, int* x, int len) 
-// bool = 0 (false) => vetor x tem algum valor 0, do contrário: bool = 1 (true)
+int m[9][9]; // matriz pra armazenar o sudoku
+int isTrue; // int pra marcar a veracidade da soluçao: 1 = true, 0 = false;
+
+
+int alLinha (int linha)
+// analiza repetição de algarismos na LINHA
+// return 1 => não tem : return 0 => tem repetição
 {
-    for (int i = 0; i < len; i++)
-    {
-        if (x[i] && *bool)
-            *bool = 1;
-        else
-            *bool = 0;
-        //bool = bool && x[i];
-    }
+    int algCount[10] = {};
+    for (int i = 0; i < 9; i++)
+        algCount[m[linha][i]]++;
+    
+    for (int i = 1; i < 10; i++)
+        if (!algCount[i])
+            return 0;
+
+    return 1;
 }
 
-int main ()
+int alColuna (int coluna)
+// analiza repetição de algarismos na COLUNA
+// return 1 => não tem : return 0 => tem repetição
 {
-    // quantidade de tabuleiros a verificar
-    int numTabuleiros, instancia;
-    scanf("%d", &numTabuleiros);
-    int m[9][9];
+    int algCount[10] = {};
+    for (int i = 0; i < 9; i++)
+        algCount[m[i][coluna]]++;
+    
+    for (int i = 1; i < 10; i++)
+        if (!algCount[i])
+            return 0;
+    
+    return 1;
+}
 
-    for (instancia = 1; instancia <= numTabuleiros; instancia++)
+int al3x3(int linha3x3, int coluna3x3)
+{
+    // linha3x3 = {0,1,2}
+    // coluna3x3 = {0,1,2}
+    // analiza repetição em blocos 3x3 da matriz m
+    // return 0 => há algarismo repetido : return 1 => não há repetição
+    int algCount[10] = {};
+    for (int i = 0 + linha3x3*3; i < 3 + linha3x3*3; i++)
+        for (int j = 0 + coluna3x3*3; j < 3 + coluna3x3*3; j++)
+        {
+            algCount[m[i][j]]++;
+        }
+    for (int i = 1; i < 10; i++)
     {
-        int isTrue = 1; // algo como uma boolean
-        // 1 => resolução viável 
-        // 0 => resolução não viável
-
-        // input matriz
-        for (int i = 0; i < 10; i++)
-        {
-            scanf("%d %d %d %d %d %d %d %d %d", &m[i][0], &m[i][1], &m[i][2], &m[i][3], &m[i][4], &m[i][5], &m[i][6], &m[i][7], &m[i][8]);
-        }
-
-        // array cheio de 0 (false)
-        int count[10]; 
-        // isTrue = isTrue && count[i]; // codigo pra ser adicionado ao final
-        int counter = 0;
-        while (1)
-        {
-            for (int i = 0; i < 10; i++)
-            {
-                count[i] = 0;
-            }
-            int lin = 0, linMax = 2, col = 0, colMax = 2;
-            while (lin <= linMax)
-            {
-                while (col <= colMax)
-                {
-                    // soma 1 pra tornar verdadeiro. Se um número repetir algo vai ficar como false
-                    count[m[lin][col]]++;
-                }
-            }
-
-            // verificar veracidade do 3 x 3
-            verify (&isTrue, count, 10);
-            if (isTrue == 0)
-                break;
-            
-            counter++;
-            if (counter == 3 || counter == 6)
-            {
-                lin += 3;
-                linMax += 3;
-                col = 0;
-                colMax = 2;
-            }
-            if (counter == 9)
-            {
-                break;
-            }
-        }
-
-        // verificar linhas
-        if (isTrue)
-            for (int i = 0; i < 10; i++)
-            {
-                for (int j = 0; j < 10; j++)
-                {
-                    count[j] = 0;
-                }
-                for (int j = 0; j < 10; j++)
-                {
-                    // soma 1 pra tornar verdadeiro. Se um número repetir algo vai ficar como false
-                    count[m[i][j]]++;
-                }
-                verify (&isTrue, count, 10);
-                if (isTrue == 0)
-                    break;
-            }
-
-        // verificar colunas
-        if (isTrue)
-            for (int j = 0; j < 10; j++)
-            {
-                for (int i = 0; i < 10; i++)
-                {
-                    count[i] = 0;
-                }
-                for (int i = 0; i < 10; i++)
-                {
-                    // soma 1 pra tornar verdadeiro. Se um número repetir algo vai ficar como false
-                    count[m[i][j]]++;
-                }
-                verify (&isTrue, count, 10);
-                if (isTrue == 0)
-                    break;
-            }
-
-        printf ("Instancia %d\n", instancia);
-        if (isTrue == 1)
-            printf ("SIM\n\n");
-        else 
-            printf("NAO\n\n");
+        if (!algCount[i])
+            return 0;
     }
-    return 0;
+    return 1;
+}
+
+int main()
+{  
+    int k; // le o numero de soluções a serem conferidas
+    scanf("%d", &k);
+
+    for (int instancia = 1; instancia <= k; instancia++)
+    {
+        isTrue = 1;
+        // loop pra analizar cada solução
+        for (int i = 0; i < 9; i++)
+            for (int j = 0; j < 9; j++)
+            {
+                // loop pra dar entrada na solução
+                scanf("%d", &m[i][j]);
+            }
+
+        printf("Instancia %d\n", instancia);
+        
+        for (int i = 0; i < 9; i++)
+        {
+            // analizar as colunas
+            isTrue = alColuna(i);
+            if (!isTrue)
+            {
+                printf("NAO\n\n");
+                break;
+            }
+        }
+        if (!isTrue)
+            continue;
+
+        for (int i = 0; i < 9; i++)
+        {
+            // analizar as linhas
+            isTrue = alLinha(i);
+            if (!isTrue)
+            {
+                printf("NAO\n\n");
+                break;
+            }
+        }
+        if (!isTrue)
+            continue;
+
+
+        // analizar 3x3
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                isTrue = al3x3(i, j);
+                if (!isTrue)
+                {
+                    printf("NAO\n\n");
+                    break;
+                }
+            }
+            if (!isTrue)
+                break;
+        }
+        if (!isTrue)
+            continue;
+        
+        printf("SIM\n\n");
+    }
 }
