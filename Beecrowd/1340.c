f84f1a8e0890c3c9e5cdbb35faef8339e0bd8d47
Leonardo Arantes Lopes Macedo <macedoleonardo870@gmail.com>
2024-12-05 16:55:29 -0300

    nao concluido

--- Beecrowd/1340.c
+++ Beecrowd/1340.c
@@ -2,69 +2,125 @@
 #include <stdlib.h>
 #include <string.h>
 
-int max (int v[], n) {
-    int x = v[0];
-    for (int i = 1; i < n; i++) {
-        if (v[i] > x)
-            x = v[i];
+int maxIndex (int v[], int n) {
+    int max = v[0], index = 0;
+    for (int i = 1; i < n; i++)
+        if (v[i] > max) {
+            max = v[i];
+            index = i;
+        }
+    return index;
+}
+
+/* (v[], 0, len)
+void OrganizeR (int v[], int i) {
+    int temp;
+    if (i <= 500) {
+        if (v[i*2] > v[i]) {
+            temp = v[i];
+            v[i] = v[i*2];
+            v[i*2] = temp;
+        }
+        if (v[i*2+1] > v[i]) {
+            temp = v[i];
+            v[i] = v[i*2];
+            v[i*2] = temp;
+        }
+        return OrganizeR (v, i + 1);
     }
-    return x;
 }
+void queueOrganize (int v[]) {
+    OrganizeR (v, 0);
+}*/
 
-void pushStack (int v[], int * f, int x) {
-    v[++*f] = x;
-}
-
-// v[] só tem int > 0 && < 100
-void popStack (int v[], int * f) {
-    v[*f--] = 0;
+int compareInts(const void* a, const void* b) {
+    int x = *(const int*)a;
+    int y = *(const int*)b;
+    if (x > y) return -1;
+    else if (x < y) return 1;
+    return 0;
 }
 
 void pushQueue (int v[], int * f, int x) {
-    v[++*f] = x;
+    v[*f++] = x;
 }
 
-void popQueue (int v[], int * r) {
-    v[*f--] = 0;
+void pushPQueue (int v[], int * f, int x) {
+    v[*f++] = x;
+    qsort (v, 1000, sizeof(int), compareInts);
+    //queueOrganize (v);
 }
 
-void PQueueOrganize ()
-
-void pushPQueue (int v[], int * f, int n) {
-    
+void pushStack (int v[], int * f, int x) {
+    v[*f++] = x;
 }
 
-void popPQueue (int v[], int * f, int n)
+int popQueue (int v[], int * r) {
+    int temp = v[--*r];
+    v[*r] = 0;
+    return temp;
+}
+
+int popPQueue (int v[], int * f) {
+    int temp = v[0];
+    v[0] = 0;
+    *f--;
+    qsort (v, 1000, sizeof(int), compareInts);
+    return temp;
+}
+
+// v[] só tem int > 0 && < 100
+int popStack (int v[], int * f) {
+    int temp = v[*f];
+    v[--*f] = 0;
+    return temp;
+}
+
+void clearVet (int v[], int n) {
+    v[--n] = 0;
+    if (n != -1)
+        return clearVet (v, n-1);
+}
 
 int main () {
     int n, acao, valor;
-    int fila[1000], r, f;
-    int fPrio[1000], r, f;
-    int pilha[1000], f;
-    int fPrioBool = 1, fBool = 1, pilhaBool = 1;
+    int queue[1000], rQ = 0, fQ = 0;
+    int pQueue[1000], fPQ = 0;
+    int stack[1000], fS = -1;
+    int queueBool = 1, pQueueBool = 1, stackBool = 1;
+
     while (scanf ("%d", &n) != EOF) {
-        r = 0;
-        f = 0;
+        queueBool = 1, pQueueBool = 1, stackBool = 1;
+        clearVet (queue, 1000);
+        clearVet (pQueue, 1001);
+        clearVet (stack, 1000); 
+        rQ = 0, fQ = -1, fPQ = 0, fS = -1;
         for (int i = 0; i < n; i++) {
             scanf ("%d %d", &acao, &valor);
             if (acao == 1) {
-
+                pushQueue (queue, &fQ, valor);
+                pushPQueue (pQueue, &fPQ, valor);
+                pushStack (stack, &fS, valor);
             }
             if (acao == 2) {
-                if (fPrioBool)
-                    if (lista!= max (lista, f))
-                        fPrioBool--;
-                if (fila)
-                    if (valor != lista[r])
-                        fila--;
-                if (pilha)
-                    if (valor != lista[f])
-                        pilha--;
-                if (pilha && fila)
+                if (popQueue (queue, &rQ) != valor)
+                    queueBool--;
+                if (popPQueue (pQueue, &fPQ) != valor)
+                    pQueueBool--;
+                if (popStack (stack, &fS) != valor)
+                    stackBool--;
             }
         }
-        //
+        if (stackBool == 1)
+            printf ("stack\n");
+        else if (queueBool == 1 && !(pQueueBool == 1))
+            printf ("queue\n");
+        else if (pQueueBool == 1 && !(queueBool == 1))
+            printf ("priority queue\n");
+        else if (queueBool == 1 && pQueueBool == 1)
+            printf ("not sure\n");
+        else
+            printf ("impossible  %d  %d  %d\n", queueBool, pQueueBool, stackBool);
     }
-
     return 0;
 }
