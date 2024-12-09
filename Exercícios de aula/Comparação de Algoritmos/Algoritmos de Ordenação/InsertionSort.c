2aacb954b9976f8206f11403e674be08f3fb59d4
Leonardo Arantes Lopes Macedo <macedoleonardo870@gmail.com>
2024-12-09 00:25:47 -0300

    Create InsertionSort.c

---
+++ Exercícios de aula/Comparação de Algoritmos/Algoritmos de Ordenação/InsertionSort.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h> 
+
+const int n = 400000; 
+
+void insertionSort (int n, int v[]) {
+    int i, j, x;
+    for (j = 1;j < n; j++) {
+            x = v[j];
+        for (i = j-1; i >= 0 && v[i] > x; i--)
+            v[i+1] = v[i];
+        v[i+1] = x;
+    }
+}
+
+int main(){
+    clock_t t;
+    int vetor[n];
+    
+    //semente de aleatoriedade
+    srand ((unsigned)time(NULL));
+
+    //geração aleatório dos valores do vetor
+    for (int a = 0; a < n; a++)
+        vetor[a] = rand() % n; 
+    
+    t = clock(); 
+
+    insertionSort (n, vetor);
+
+    t = clock() - t;
+
+    printf("Tempo de execução: %lf ms\n", (((double)t)/((CLOCKS_PER_SEC/1000))));
+
+    return 0;
+}
