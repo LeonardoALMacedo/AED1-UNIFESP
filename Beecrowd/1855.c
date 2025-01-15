ea9addfab6b07404025b9b186eddd104811ca07c
Leonardo Arantes Lopes Macedo <macedoleonardo870@gmail.com>
2025-01-15 11:52:20 -0300

    Mapa do Meistre

--- Beecrowd/1855.c
+++ Beecrowd/1855.c
@@ -1,80 +1,62 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
 int x, y;
 
-void percorreDireita (int *i, int *j, char mapa[x][y]) {
-    while (*i < x) {
-        if (mapa[*i][*j] != '.')
-            break;
-        *i++;
-    }
-}
-
-void percorreEsquerda (int *i, int *j, char mapa[x][y]) {
-    while (*i >= 0) {
-        if (mapa[*i][*j] != '.')
-            break;
-        *i--;
-    }
-}
-
-void percorreCima (int *i, int *j, char mapa[x][y]) {
-    while (*j >= 0) {
-        if (mapa[*i][*j] != '.')
-            break;
-        *j--;
-    }
-}
-
-void percorreBaixo (int *i, int *j, char mapa[x][y]) {
-    while (*j < y) {
-        if (mapa[*i][*j] != '.')
-            break;
-        *j++;
-    }
-}
-
 int main () {
+    scanf ("%d %d\n", &x, &y);
 
-    scanf ("%d %d", &x, &y);
-
-    char mapa[x][y];
+    char mapa[100][100];
 
     for (int i = 0; i < y; i++)
-        for (int j = 0; j < x; j++)
-            scanf ("%d", &mapa[i][j]);
+        gets (mapa[i]);
 
     int i = 0, j = 0;
+
     while (1) {
-        if (i >= x || j >= y || mapa[i][j] == '0') {
-            printf ("!\n");
+        if (i < 0 || j < 0 || i >= y || j >= x) {
+            printf("!\n");
             break;
         }
-        else if (mapa[i][j] == '*') {
-            printf ("*\n");
+
+        char atual = mapa[i][j];
+
+        if (atual == '*') {
+            printf("*\n");
             break;
         }
-        else if (mapa[i][j] == '>') {
-            mapa[i++][j] = '0';
-            percorreDireita (&i, &j, mapa);
+        else if (atual == '0') {
+            printf("!\n");
+            break;
         }
-        else if (mapa[i][j] == '<') {
-            mapa[i--][j] = '0';
-            percorreEsquerda (&i, &j, mapa);
+
+        mapa[i][j] = '0';
+
+        if (atual == '>') {
+            while (j + 1 < x && mapa[i][j + 1] == '.')
+                j++;
+            j++;
         }
-        else if (mapa[i][j] == '^') {
-            mapa[i][j--] = '0';
-            percorreCima (&i, &j, mapa);
+        else if (atual == '<') {
+            while (j - 1 >= 0 && mapa[i][j - 1] == '.')
+                j--;
+            j--;
         }
-        else if (mapa[i][j] == 'v') {
-            mapa[i][j++] = '0';
-            percorreBaixo (&i, &j, mapa);
+        else if (atual == '^') {
+            while (i - 1 >= 0 && mapa[i - 1][j] == '.')
+                i--;
+            i--;
+        }
+        else if (atual == 'v') {
+            while (i + 1 < y && mapa[i + 1][j] == '.')
+                i++;
+            i++;
+        }
+        else {
+            printf("!\n");
+            break;
         }
     }
-    
+
     return 0;
-
-
 }
